be3dfc9b4e18dd394d8f44f8c524b916fc9fffd3
Colin Sanders <colin@Colins-MacBook-Pro.local>
2021-11-16 17:01:42 -0600

    migrating to desktop

--- allgather1.c
+++ allgather1.c
@@ -3,7 +3,23 @@
 #include <mpi.h>
 #include <assert.h>
 
-int allgather(void *sendbuf, int sendcount, MPI_Datatype sendtype, void *recvbuf, int
-recvcount, MPI_Datatype recvtype, MPI_Comm comm) {
+// Gather all data elements 
+int allgather(void *sendbuf, int sendcount, MPI_Datatype sendtype, void *recvbuf, int recvcount, MPI_Datatype recvtype, MPI_Comm comm)
+{
+    int rank, size, i, offset;
+    MPI_Status *status;
+    MPI_Request *request;
+    MPI_Aint lb, sizeofsendtype, sizeofrecvtype;
 
+    MPI_Comm_rank(comm, &rank);
+    MPI_Comm_size(comm, &size);
+
+    status = malloc(sizeof(MPI_Status) * (size + 1));
+    request = malloc(sizeof(MPI_Request) * (size + 1));
+
+    //start by having every process send theirs to the root
+    if(rank != root){
+        MPI_Type_get_extend(sendtype, &lb, &sizeofsendtype);
+        offset = sizeofsendtype * sendcount * i;
+    }
 }
